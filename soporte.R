231a938abec741f70f5e83521065afebb62bfa36
Víctor Gauto <victor.gauto@outlook.com>
2025-01-17 10:51:40 -0300

    agrego script soporte

---
+++ soporte.R
@@ -0,0 +1,15 @@
+
+sep <- "<b style='color:red;'>|</b>"
+ubicacion <- "<span style='color:red'>{{< fa location-dot size=large >}}</span>"
+calendario <- "<span style='color:red'>{{< fa calendar-days size=large >}}</span>"
+titulo <- "<span style='color:red'>{{< fa user-graduate size=large >}}</span>"
+institucion <- "<span style='color:red'>{{< fa building-columns size=large >}}</span>"
+
+f_edu <- function(tit, ins, ubi, cal) {
+  paste(
+    titulo, tit, "<br>",
+    institucion, ins, "<br>",
+    ubicacion, ubi, "<br>",
+    calendario, cal, "<br>"
+  )
+}
