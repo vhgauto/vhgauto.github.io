535b6d52390118b6031cb0c8bbe41e5bee34949e
Víctor Gauto <victor.gauto@outlook.com>
2025-10-05 12:06:54 -0300

    indentación

--- favicon.R
+++ favicon.R
@@ -1,14 +1,23 @@
 png(
-  "img/favicon.png", width = 500, height = 500, units = "px", type = "cairo",
+  "img/favicon.png",
+  width = 500,
+  height = 500,
+  units = "px",
+  type = "cairo",
   bg = "#F6B40E"
 )
 terra::vect(
-  data.frame(x = c(0, 1), y = c(0, 1)), geom = c("x", "y")
+  data.frame(x = c(0, 1), y = c(0, 1)),
+  geom = c("x", "y")
 ) |>
   terra::ext() |>
   terra::as.polygons() |>
   terra::plot(
-    col = "#F6B40E", axes = FALSE, border = NA, xlim = c(0, 1), ylim = c(0, 1),
+    col = "#F6B40E",
+    axes = FALSE,
+    border = NA,
+    xlim = c(0, 1),
+    ylim = c(0, 1),
     mar = c(0, 0, 0, 0)
   )
 dev.off()
