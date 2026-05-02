c7a62e212ad8d1c01277ea08bd36195ed5610ddb
vhgauto <victor.gauto@outlook.com>
2026-05-02 19:26:36 -0300

    actualizo para incluir año en poster

--- .letterboxd.R
+++ .letterboxd.R
@@ -6,10 +6,20 @@ f_poster <- function(id) {
   l <- TMDb::search_movie(
     api_key = Sys.getenv("api_key_TMDb"),
     query = m$movie[id]
-  )$results$poster_path[1]
+  ) |>
+    purrr::pluck("results") |>
+    tibble::as_tibble() |>
+    tidyr::drop_na(release_date) |>
+    dplyr::mutate(release_date = as.Date(release_date))
+
+  p <- dplyr::select(l, release_date, poster_path) |>
+    tidyr::drop_na() |>
+    dplyr::mutate(d = lubridate::year(release_date) - m$year[id]) |>
+    dplyr::slice_min(order_by = abs(d), n = 1) |>
+    dplyr::pull(poster_path)
 
   download.file(
-    url = paste0("https://image.tmdb.org/t/p/original/", l),
+    url = paste0("https://image.tmdb.org/t/p/original/", p),
     mode = "wb",
     quiet = TRUE,
     destfile = paste0("img/poster_", id, ".jpg")
